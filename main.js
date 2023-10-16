14948f05c61cfea2323eb11b213adf2f62371cea
Calm Matt <40216910+mattkje@users.noreply.github.com>
2023-10-16 21:51:35 +0200

    updated main.js

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+/**
+ * Checks if device is mobile, and redirects the device to mobile.html
+ * if device is mobile.
+ */
 if (/Mobi|Android/i.test(navigator.userAgent)) {
-    window.location.href = 'mobile.html'; // Redirect to unsupported.html
+    window.location.href = 'mobile.html';
 }
+
+/**
+ * This method should display current date (weekday, month, day, year)
+ */
 function displayCurrentDate() {
     var currentDate = new Date();
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -9,26 +17,21 @@ function displayCurrentDate() {
     document.getElementById('currentDate').textContent = formattedDate;
 }
 
+/**
+ * This method should display a countdown in an HTML element from
+ * a set date.
+ */
 function updateCountdown() {
-    // Define the target date (01.12.23 in dd.mm.yy format)
     const targetDate = new Date('2023-12-01');
-
-    // Get the current date
     const currentDate = new Date();
 
-    // Calculate the time difference in milliseconds
     const timeDifference = targetDate - currentDate;
-
-    // Calculate the number of days
     const daysRemaining = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
-    // Display the countdown in an HTML element
     document.getElementById('countdown').textContent = daysRemaining + " days";
 }
 
-// Update the current date and countdown initially
+
 displayCurrentDate();
 updateCountdown();
-
-// Update the countdown every second
 setInterval(updateCountdown, 1000);
